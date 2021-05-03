8a72d8159162fa84cce8f7bf454c501f27972778
MHD <mohammad.alzabibi@gmail.com>
2021-05-03 15:40:29 +0300

    'init'

---
+++ app/etc/env.php
@@ -0,0 +1,72 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'e672166f9f27583357b25c43c814d7d3'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'e_store',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1',
+                'driver_options' => [
+
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '09f_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '09f_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 23 Apr 2021 19:28:43 +0000'
+    ],
+    'downloadable_domains' => [
+        'www.text.com'
+    ]
+];
